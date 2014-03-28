83f6c58018639155da2e58b1fe6fb715564abea8
CatfishStudio <somov@i.ua>
2014-03-28 22:28:03 +0200

    Update: Menu: 28-03-2014  22:27

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.справочникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.авторыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.работникиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.спектаклиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.документыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.билетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.жуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.труппыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.репертуарToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.местаВЗалеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходИзПрограммыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справочникиToolStripMenuItem,
+            this.документыToolStripMenuItem,
+            this.жуToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходИзПрограммыToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.файлToolStripMenuItem.Text = "Файл.";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 540);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(112, 17);
+            this.toolStripStatusLabel1.Text = "2014 (с) Сомов Е.П.";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(784, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // справочникиToolStripMenuItem
+            // 
+            this.справочникиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.авторыToolStripMenuItem,
+            this.работникиToolStripMenuItem,
+            this.спектаклиToolStripMenuItem,
+            this.местаВЗалеToolStripMenuItem});
+            this.справочникиToolStripMenuItem.Name = "справочникиToolStripMenuItem";
+            this.справочникиToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.справочникиToolStripMenuItem.Text = "Справочники";
+            // 
+            // авторыToolStripMenuItem
+            // 
+            this.авторыToolStripMenuItem.Name = "авторыToolStripMenuItem";
+            this.авторыToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.авторыToolStripMenuItem.Text = "Авторы";
+            // 
+            // работникиToolStripMenuItem
+            // 
+            this.работникиToolStripMenuItem.Name = "работникиToolStripMenuItem";
+            this.работникиToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.работникиToolStripMenuItem.Text = "Работники";
+            // 
+            // спектаклиToolStripMenuItem
+            // 
+            this.спектаклиToolStripMenuItem.Name = "спектаклиToolStripMenuItem";
+            this.спектаклиToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.спектаклиToolStripMenuItem.Text = "Спектакли";
+            // 
+            // документыToolStripMenuItem
+            // 
+            this.документыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.билетToolStripMenuItem});
+            this.документыToolStripMenuItem.Name = "документыToolStripMenuItem";
+            this.документыToolStripMenuItem.Size = new System.Drawing.Size(82, 20);
+            this.документыToolStripMenuItem.Text = "Документы";
+            // 
+            // билетToolStripMenuItem
+            // 
+            this.билетToolStripMenuItem.Name = "билетToolStripMenuItem";
+            this.билетToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.билетToolStripMenuItem.Text = "Билет";
+            // 
+            // жуToolStripMenuItem
+            // 
+            this.жуToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.труппыToolStripMenuItem,
+            this.репертуарToolStripMenuItem});
+            this.жуToolStripMenuItem.Name = "жуToolStripMenuItem";
+            this.жуToolStripMenuItem.Size = new System.Drawing.Size(72, 20);
+            this.жуToolStripMenuItem.Text = "Журналы";
+            // 
+            // труппыToolStripMenuItem
+            // 
+            this.труппыToolStripMenuItem.Name = "труппыToolStripMenuItem";
+            this.труппыToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.труппыToolStripMenuItem.Text = "Труппы";
+            // 
+            // репертуарToolStripMenuItem
+            // 
+            this.репертуарToolStripMenuItem.Name = "репертуарToolStripMenuItem";
+            this.репертуарToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.репертуарToolStripMenuItem.Text = "Репертуар";
+            // 
+            // местаВЗалеToolStripMenuItem
+            // 
+            this.местаВЗалеToolStripMenuItem.Name = "местаВЗалеToolStripMenuItem";
+            this.местаВЗалеToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.местаВЗалеToolStripMenuItem.Text = "Места в зале";
+            // 
+            // выходИзПрограммыToolStripMenuItem
+            // 
+            this.выходИзПрограммыToolStripMenuItem.Name = "выходИзПрограммыToolStripMenuItem";
+            this.выходИзПрограммыToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.выходИзПрограммыToolStripMenuItem.Text = "Выход из программы";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 562);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Театр v 1.0";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справочникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem авторыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem работникиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem спектаклиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem документыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem билетToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem жуToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem местаВЗалеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem труппыToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem репертуарToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходИзПрограммыToolStripMenuItem;
     }
 }
 
---
+++ SqlServerFull.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using System.Data.Sql;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Theater
+{
+    class SqlServerFull
+    {
+    }
+}
